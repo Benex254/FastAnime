cae3c307e7e886cbf0be965eda1dcc974ed0e8b0
Benex254 <benextempest@gmail.com>
2024-08-03 13:40:56 +0300

    feat(anime provider): improve browser impersonation by using sessions

--- fastanime/libs/anime_provider/__init__.py
+++ fastanime/libs/anime_provider/__init__.py
@@ -2,14 +2,3 @@ from .allanime.api import AllAnimeAPI
 from .animepahe.api import AnimePaheApi
 
 anime_sources = {"allanime": AllAnimeAPI, "animepahe": AnimePaheApi}
-
-
-class Anime_Provider:
-    def search_for_anime(self):
-        pass
-
-    def get_anime(self):
-        pass
-
-    def get_episode_streams(self):
-        pass

5c934d3831a7838f199deed81e1b944520c893ba
Benex254 <benextempest@gmail.com>
2024-08-05 09:47:04 +0300

    feat: ensure correct python version

--- fastanime/__init__.py
+++ fastanime/__init__.py
@@ -1,6 +1,12 @@
+import sys
+
+if sys.version_info < (3, 10):
+    raise ImportError(
+        "You are using an unsupported version of Python. Only Python versions 3.8 and above are supported by yt-dlp"
+    )  # noqa: F541
+
 import logging
 import os
-import sys
 
 from dotenv import load_dotenv
 

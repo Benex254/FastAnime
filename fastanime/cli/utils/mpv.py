1f34e91da869da09056ecc3b9827e4003e8b3ea7
Benex254 <benextempest@gmail.com>
2024-08-05 09:47:02 +0300

    feat: make the mpv player module work on android

--- fastanime/cli/utils/mpv.py
+++ fastanime/cli/utils/mpv.py
@@ -5,6 +5,19 @@ import subprocess
 def mpv(link, title, *custom_args):
     MPV = shutil.which("mpv")
     if not MPV:
-        print("mpv not found")
-        return
-    subprocess.run([MPV, *custom_args, f"--title={title}", link])
+        args = [
+            "nohup",
+            "am",
+            "start",
+            "--user",
+            "0",
+            "-a",
+            "android.intent.action.VIEW",
+            "-d",
+            link,
+            "-n",
+            "is.xyz.mpv/.MPVActivity",
+        ]
+        subprocess.run(args)
+    else:
+        subprocess.run([MPV, *custom_args, f"--title={title}", link])

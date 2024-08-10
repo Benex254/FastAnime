107f398483a1296889c20c8d0f2e686be903eb67
SilvioDylan <kirkeksibir@gmail.com>
2024-08-11 01:46:41 +0200

    Keep window size stable when changing episode

--- fastanime/cli/utils/player.py
+++ fastanime/cli/utils/player.py
@@ -152,7 +152,6 @@ class MpvPlayer(object):
             ytdl=True,
         )
         mpv_player.force_window = config.force_window
-        mpv_player._set_property("auto-window-resize", "no") # Prevents MPV from resizing the window when a new video is loaded. Useful for tiling window managers. Does not affect fullscreen.
         # mpv_player.cache = "yes"
         # mpv_player.cache_pause = "no"
         mpv_player.title = title
@@ -192,6 +191,7 @@ class MpvPlayer(object):
         # -- keybindings --
         @mpv_player.on_key_press("shift+n")
         def _next_episode():
+            mpv_player._set_property("auto-window-resize", "no") # Prevents MPV from resizing the window when a new video is loaded.
             url = self.get_episode("next")
             if url:
                 mpv_player.loadfile(url, 
@@ -200,6 +200,7 @@ class MpvPlayer(object):
 
         @mpv_player.on_key_press("shift+p")
         def _previous_episode():
+            mpv_player._set_property("auto-window-resize", "no") # Prevents MPV from resizing the window when a new video is loaded.
             url = self.get_episode("previous")
             if url:
                 mpv_player.loadfile(

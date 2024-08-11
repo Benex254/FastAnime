1ead2fb176686c0229b34d55db4e0bec5cb7448e
Benex254 <benextempest@gmail.com>
2024-08-11 21:09:18 +0300

    chore(cli): get rid of depracted function

--- fastanime/cli/utils/utils.py
+++ fastanime/cli/utils/utils.py
@@ -2,9 +2,6 @@ import logging
 from typing import TYPE_CHECKING
 
 from InquirerPy import inquirer
-from thefuzz import fuzz
-
-from ...Utility.data import anime_normalizer
 
 logger = logging.getLogger(__name__)
 if TYPE_CHECKING:
@@ -101,30 +98,3 @@ def fuzzy_inquirer(choices: list, prompt: str, **kwargs):
         **kwargs,
     ).execute()
     return action
-
-
-# WARNING: This function is depracated use the one from the main utility package
-def anime_title_percentage_match(
-    possible_user_requested_anime_title: str, title: tuple
-) -> float:
-    """Returns the percentage match between the possible title and user title
-
-    Args:
-        possible_user_requested_anime_title (str): an Animdl search result title
-        title (str): the anime title the user wants
-
-    Returns:
-        int: the percentage match
-    """
-    if normalized_anime_title := anime_normalizer.get(
-        possible_user_requested_anime_title
-    ):
-        possible_user_requested_anime_title = normalized_anime_title
-    for key, value in locals().items():
-        logger.info(f"{key}: {value}")
-    # compares both the romaji and english names and gets highest Score
-    percentage_ratio = max(
-        fuzz.ratio(title[0].lower(), possible_user_requested_anime_title.lower()),
-        fuzz.ratio(title[1].lower(), possible_user_requested_anime_title.lower()),
-    )
-    return percentage_ratio

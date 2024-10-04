58761f5b961690e6305907b2b0edd37e3a797843
Benex254 <benextempest@gmail.com>
2024-10-04 19:44:54 +0300

    chore: bump version

--- fastanime/__init__.py
+++ fastanime/__init__.py
@@ -2,11 +2,11 @@ import sys
 
 if sys.version_info < (3, 10):
     raise ImportError(
-        "You are using an unsupported version of Python. Only Python versions 3.8 and above are supported by yt-dlp"
+        "You are using an unsupported version of Python. Only Python versions 3.10 and above are supported by FastAnime"
     )  # noqa: F541
 
 
-__version__ = "v2.6.1"
+__version__ = "v2.6.2"
 
 APP_NAME = "FastAnime"
 AUTHOR = "Benex254"

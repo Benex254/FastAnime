cac7586a86dee5c7e44bb51d113e2539ba76b6d7
benex <benextempest@gmail.com>
2024-12-02 11:42:56 +0300

    refactor(constants): update API_ENDPOINT to use f-string for improved readability

--- fastanime/libs/anime_provider/allanime/constants.py
+++ fastanime/libs/anime_provider/allanime/constants.py
@@ -1,4 +1,4 @@
 SERVERS_AVAILABLE = ["sharepoint", "dropbox", "gogoanime", "weTransfer", "wixmp", "Yt"]
 API_BASE_URL = "allanime.day"
 API_REFERER = "https://allanime.to/"
-API_ENDPOINT = "https://api.{}/api/".format(API_BASE_URL)
+API_ENDPOINT = f"https://api.{API_BASE_URL}/api/"

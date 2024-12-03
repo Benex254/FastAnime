1ff3074fadd51700517acb4340ca9bbb11f1db80
Gand0rf <gand0rf@protonmail.com>
2024-12-02 20:11:40 -0500

    updated discord.py

--- fastanime/libs/discord/discord.py
+++ fastanime/libs/discord/discord.py
@@ -3,9 +3,9 @@ import time
 
 def discord_connect(show, episode, switch):
     presence = Presence(client_id = '1292070065583165512')
+    presence.connect()
     if not switch.is_set():
-        presence.update(
-                details = show,
-                state = "Watching "+episode
-        )
+        presence.update(details = show, state = "Watching "+episode)
         time.sleep(10)
+    else:
+        presence.close()

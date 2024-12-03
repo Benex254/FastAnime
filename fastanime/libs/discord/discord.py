52af68d13fabf65980028604d64543f469aa920c
Gand0rf <gand0rf@protonmail.com>
2024-12-02 20:26:49 -0500

    minor chagne to discord.py

--- fastanime/libs/discord/discord.py
+++ fastanime/libs/discord/discord.py
@@ -5,7 +5,7 @@ def discord_connect(show, episode, switch):
     presence = Presence(client_id = '1292070065583165512')
     presence.connect()
     if not switch.is_set():
-        presence.update(details = show, state = "Watching "+episode)
+        presence.update(details = show, state = "Watching episode "+episode)
         time.sleep(10)
     else:
         presence.close()
